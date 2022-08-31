04bd7ee1e39cd44cb5d5b08ad478fb1b103b3d71
manndm4 <manndm4@gmail.com>
2022-08-31 19:18:04 -0400

    initial commit

--- BetWell/ContentView.swift
+++ BetWell/ContentView.swift
@@ -10,7 +10,7 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+//testing
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
